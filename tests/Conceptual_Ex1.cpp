3f3d807b9140194099be753426f2aeb1e02d40b4
WhiteSunOfSpace <113097854+WhiteSunOfSpace@users.noreply.github.com>
2024-05-20 00:41:29 +0300

    Update Conceptual_Ex1.cpp

--- tests/Conceptual_Ex1.cpp
+++ tests/Conceptual_Ex1.cpp
@@ -1,11 +1,6 @@
 #include <ConceptualExample01.cpp>
 #include <gtest/gtest.h>
 
-TEST(conceptual_Ex1, TEST){
-
-  using namespace ConceptualExample01;
-  MediatorBase* m;
-  ColleagueBase* sender, getter;
-  
+TEST(conceptual_Ex1, TEST1){
   EXPECT_EQ(1, 1);
 }
