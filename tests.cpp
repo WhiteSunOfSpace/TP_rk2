3b173ccf443540bb3407609e01b804b9744b4fc7
WhiteSunOfSpace <113097854+WhiteSunOfSpace@users.noreply.github.com>
2024-05-20 02:15:43 +0300

    Update tests.cpp

--- tests.cpp
+++ tests.cpp
@@ -9,19 +9,16 @@ TEST(ChatRoom, test1){
 }
 
 TEST(ChatRoom, test2){
-    using namespace ChatRoomMediatorPattern;
-
-    std::shared_ptr<Person> Nikita{ std::make_shared<Person>("Nikita") };
-    std::shared_ptr<Person> Max{ std::make_shared<Person>("Max") };
-
-    std::shared_ptr<GoogleChat> room{ std::make_shared<GoogleChat>() };
-
-    room->join(Nikita);
-    room->join(Max);
-	
-    std::string f = Nikita->say("Hi");
-    std::string s = Max->say("Hi");
-
-    EXPECT_EQ(f, s);
+	using namespace ChatRoomMediatorPattern;
+		
+	std::shared_ptr<Person> Nikita{ std::make_shared<Person>("Nikita") };
+	std::shared_ptr<Person> Max{ std::make_shared<Person>("Max") };
+		
+	std::shared_ptr<GoogleChat> room{ std::make_shared<GoogleChat>() };
+		
+	room->join(Nikita);
+	room->join(Max);
+		
+	EXPECT_EQ(Nikita->say("Hi"), Max->say("Hi"));
 }
 
